012cce6d34042dfad60fbe65eefae66cfc039053
Rhegis Marques <rhegismarques@gmail.com>
2024-03-15 13:26:49 -0300

    first commit

---
+++ my-app/App.js
@@ -0,0 +1,23 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View } from "react-native";
+import Title from "./src/componets/Title";
+import Form from "./src/componets/Form";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <StatusBar style="auto" />
+      <Title />
+      <Form />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: "center",
+    flex: 1,
+    backgroundColor: "#e0e5e5",
+    paddingTop: 250,
+  },
+});
